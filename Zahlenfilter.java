3ecf963847a341fbb781e5ddb0de01ff765977a8
repl.it user <replituser@example.com>
2020-10-23 09:23:30 +0000

    now also tracking new file

---
+++ Zahlenfilter.java
@@ -0,0 +1,18 @@
+class Zahlenfilter {
+  public static void main(String[] args) {
+    for(int i = 0; i < 200; i++) {
+      if(i%5 == 0) {
+        System.out.println(i + " ist durch 5 teilbar!");
+      }
+
+      if(i % 10 == 9) {
+        System.out.println(i + " endet auf 9!");
+      }
+
+      int temp = i + (i-1);
+      if(temp % 3 == 0) {
+        System.out.println(i +" und " + (i-1) + " addiert ergeben " + temp + " und " + temp + " ist durch 3 teilbar");
+      }
+    }
+  }
+}
